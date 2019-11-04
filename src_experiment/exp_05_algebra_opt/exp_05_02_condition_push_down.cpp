9e235bb58443387bdcb7028e2a10cf2afcc7e6c6
SUMOVER <sumover@foxmail.com>
2019-11-04 20:34:22 +0800

    给优化算法做了铺垫

--- src_experiment/exp_05_algebra_opt/exp_05_02_condition_push_down.cpp
+++ src_experiment/exp_05_algebra_opt/exp_05_02_condition_push_down.cpp
@@ -5,65 +5,270 @@
 #include <relationalalgebra/optimizer.h>
 #include <dongmensql/expression.h>
 
+#if 1
+
+class _Table;
+
+class _SRA;
+
+class _Join;
+
+class Select;
+
+class JoinSelect;
+
+_SRA *SRAConstruct(SRA_t *sra, SRA_t *before);
+
+bool isOperator(TokenType type);
+
+Expression *constructSingleExpression(Expression *from_expr, bool matchWord);
+
+Expression *nextBeginExpressionNode(Expression *node);
+
+std::vector<Expression *> expressionSpread(Expression *begin_expr);
+
+Expression *cutANDOperator(Expression *expr);
+
+bool isJoinExpression(Expression *expr);
+
+#endif
+
+class _SRA {    // explain:此处使用状态模式
+private:
+    SRA_t *before;
+public:
+    void setBefore(SRA_t *sra) {
+        before = sra;
+    }
+
+    SRA_t *getBefore(SRA_t *sra) {
+        return before;
+    }
+
+    virtual void setSRAOn(std::vector<Expression *> expressionVector) = 0;
+
+    virtual void pushSRADown(std::vector<Expression *> expressionVector) = 0;
+};//explain: 其实也不是状态模式, 就是活用了C++的继承而已
+
+class _Table : public _SRA {
+    SRA_t *table_sra;
+    char *table_name;
+public:
+    _Table(SRA_t *sra) {
+        table_sra = sra;
+        table_name = new_id_name();
+        strcpy(table_name, sra->table.ref->table_name);
+    }
+
+    void setSRAOn(std::vector<Expression *> expressionVector) override {
+        //explain: 漫长的算法他又lei了
+
+
+    }
+
+    void pushSRADown(std::vector<Expression *> expressionVector) override {
+
+    }
+};
+
+class _Join : public _SRA {
+    SRA_t *join_sra;
+    _SRA *left, *right;
+public:
+    _Join(SRA_t *sra) {
+        join_sra = sra;
+        right = left = nullptr;
+    }
+
+    void setLeft(_SRA *sra_left) {
+        left = sra_left;
+    }
+
+    void setRight(_SRA *sra_right) {
+        right = sra_right;
+    }
+
+    void setSRAOn(std::vector<Expression *> expressionVector) override {
+
+    }
+
+    void pushSRADown(std::vector<Expression *> expressionVector) override {
+
+    }
+};
+
+class Select {
+public:
+    explicit Select(Expression *e) : expr(e) {}
+
+protected:
+    Expression *expr;
+};
+
+class JoinSelect : public Select {
+    char *left_field_name, *left_table_name;
+    char *right_field_name, *right_table_name;
+public:
+    explicit JoinSelect(Expression *e) : Select(e) {
+        left_field_name = new_id_name();
+        right_field_name = new_id_name();
+        left_table_name = new_id_name();
+        right_table_name = new_id_name();
+//        strcpy(left_table_name,)
+    }
+};
+
 /**
- * 通过tableName来查找相关的Expression
- * @param tableName
+ * construct a _SRA object, to parse and fix the struct of SRA_t which is parse from where expression
+ * As we know, a SRA_t always be make by Project->Select->Join,
+ * so, a connect tree must separate by SRA_Join and the leaves node must be SRA_Table.
+ * @param sra
+ * @return
+ */
+_SRA *SRAConstruct(SRA_t *sra, SRA_t *before) {
+    _SRA *pointer;
+    switch (sra->t) {
+        case SRA_TABLE: {
+            pointer = new _Table(sra);
+            pointer->setBefore(before);
+        }
+        case SRA_SELECT: {
+            return SRAConstruct(sra->select.sra, sra);
+        }
+        case SRA_JOIN: {
+            _Join *_join = new _Join(sra);
+            _join->setLeft(SRAConstruct(sra->join.sra1, sra));
+            _join->setRight(SRAConstruct(sra->join.sra2, sra));
+            pointer = _join;
+            pointer->setBefore(before);
+        }
+        case SRA_PROJECT: {
+            return SRAConstruct(sra->project.sra, sra);
+        }
+        default:
+            return nullptr;
+    }
+}
+
+bool isOperator(TokenType type) {
+    TokenType types[21] = {TOKEN_OPEN_PAREN,
+                           TOKEN_CLOSE_PAREN,
+                           TOKEN_POWER,
+                           TOKEN_PLUS,
+                           TOKEN_MINUS,
+                           TOKEN_DIVIDE,
+                           TOKEN_MULTIPLY,
+                           TOKEN_LT,              //less-than operator
+                           TOKEN_GT,
+                           TOKEN_EQ,
+                           TOKEN_NOT_EQUAL,
+                           TOKEN_LE,               //less-than-or-equal-to operator"
+                           TOKEN_GE,
+                           TOKEN_IN,
+                           TOKEN_LIKE,
+                           TOKEN_AND,
+                           TOKEN_OR,
+                           TOKEN_NOT,
+                           TOKEN_ASSIGNMENT,
+                           TOKEN_FUN,
+                           TOKEN_COMMA};
+    for (int i = 0; i < 21; ++i)if (type == types[i])return true;
+    return false;
+}
+
+/**
+ * 从expression串中截取一段单独的Expression
+ * @param from_expr
+ * @return
+ */
+Expression *constructSingleExpression(Expression *from_expr, bool matchWord = false) {
+    if (!isOperator(from_expr->opType))matchWord = true;
+    if ((isOperator(from_expr->opType) && matchWord) || (from_expr->nextexpr == nullptr)) {
+        return nullptr;
+    } else {
+        Expression *expr = new Expression(from_expr->opType,
+                                          constructSingleExpression(from_expr->nextexpr, matchWord));
+        expr->term = from_expr->term;
+        expr->alias = from_expr->alias;
+    }
+}
+
+/**
+ * explain: to fund next begin expression node
+ * @param node
+ * @return
+ */
+Expression *nextBeginExpressionNode(Expression *node) {
+    if (node == nullptr)return nullptr;
+    if (!isOperator(node->opType)) {             //explain: begin not from a operator
+        Expression *pointer = node;
+        while (pointer) {
+            if (isOperator(pointer->opType))    //explain: end while find a operator
+                return pointer;
+            pointer = pointer->nextexpr;        //explain: else, go on.
+        }
+    } else {                                    //explain: begin from a operator
+        Expression *pointer = node;             //explain: to find next no-operator node
+        while (pointer && isOperator(pointer->opType)) {
+            pointer = pointer->nextexpr;
+        }
+        return nextBeginExpressionNode(pointer->nextexpr);//explain: repeat precess before
+        //complain: such a stupid process~
+    }
+}
+
+std::vector<Expression *> expressionSpread(Expression *begin_expr) {
+    std::vector<Expression *> expressionVector;
+    bool matchWord = false;
+    for (Expression *pointer = begin_expr;
+         pointer != nullptr;
+         pointer = nextBeginExpressionNode(pointer)) {  //explain: iterator the expr link
+        expressionVector.push_back(constructSingleExpression(cutANDOperator(pointer)));
+    }
+    return expressionVector;
+}
+
+/**
+ * clear all `AND` node of expression linklist.
+ * which will returned
  * @param expr
  * @return
  */
-std::vector<Expression *> search_expression_by_tableName(const char *tableName, std::vector<Expression *> &exprs) {
-    std::vector<Expression *> exprTable;
-    for (auto &exp:exprs) {
-
+Expression *cutANDOperator(Expression *expr) {
+    Expression *pointer;
+    if (expr != nullptr)pointer = expr;
+    else return expr;
+    while (pointer->nextexpr != nullptr) {
+        if (pointer->opType != TOKEN_AND)return pointer;
+        else {
+            Expression *e = pointer->nextexpr;
+            free(pointer);
+            pointer = e;
+            continue;
+        }
+        pointer = pointer->nextexpr;
     }
+    return pointer;
 }
 
-void rebuild_sra(SRA_t *sra, std::vector<Expression *> &expr) {
-    switch (sra->t) {
-        case SRA_SELECT: {
-            /* 拆分cond中的Expression
-             * 并将它们push到不是select为止
-             * */
-            SRA_t *pointer = sra;
-            while (pointer->t != SRA_SELECT) { pointer = pointer->select.sra; }
-            sra = pointer;
-            rebuild_sra(sra, expr);
-        }
-        case SRA_TABLE: {
-            /**
-             * 在他们之前构造一堆SELECT, 并且最后的SELECT要指向当前的SRA
-             */
-            const char *tableName = sra->table.ref->table_name;
-
-        }
-        case SRA_JOIN: {
-
-        }
-        case SRA_PROJECT: {
-            rebuild_sra(sra->project.sra, expr);
-        }
+bool isJoinExpression(Expression *expr) {
+    int num_TOKEN_WORD = 0;
+    Expression *pointer = expr;
+    while (pointer) {
+        if (pointer->opType == TOKEN_WORD)num_TOKEN_WORD++;
     }
+    return num_TOKEN_WORD == 2;
 }
-
-/**
-* 使用关于选择的等价变换规则将条件下推。
-*
-*/
 
 /*输入一个关系代数表达式，输出优化后的关系代数表达式
  * 要求：在查询条件符合合取范式的前提下，根据等价变换规则将查询条件移动至合适的位置。
- * TODO this function is stupid like the function Expression* Expression::expression_print(), who can tell me what the parameter `tableManager` used to?
+ * complain: this function is stupid like the function Expression* Expression::expression_print(), who can tell me what the parameter `tableManager` used to?
  * */
 SRA_t *dongmengdb_algebra_optimize_condition_pushdown(SRA_t *sra, TableManager *tableManager) {
-//    SRA_print(sra);
-//    printf("\n*************\n");
-//    sra->project.sra->select.cond->expression_print(sra->project.sra->select.cond, nullptr);
-//    printf("\n*************\n");
-    std::vector<Expression *> condExpr;
-    Expression *pointer = sra->project.sra->select.cond;
+    _SRA *join_tree = SRAConstruct(sra, nullptr);
+    Expression *begin_expr = cutANDOperator(sra->project.sra->select.cond);
+    std::vector<Expression *> expressionVector = expressionSpread(begin_expr);
 
-
-    rebuild_sra(sra->project.sra, condExpr); // 去死吧
     return sra;
 }
 
